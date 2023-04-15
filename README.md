38e25bc671b4fc3b865cad0776964d5e4fbb76e3
11ppm <goracle.moon1215i@gmail.com>
2023-04-15 21:02:11 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# goracle_backup
- 
+<br/>
+<p align="center">
+<img src="https://github.com/11ppm/pm2_log/blob/main/img/img2.jpg" width="225" alt="PluginJapan">
+</a>
+</p>
+<br/>
+
+# pli_rsync
+
+ [README 日本語版](https://github.com/11ppm/pli_rsync/blob/main/README_JP.md)
+
+## Overview
+* Create a command using rsync to transfer backup files located in the `/plinode_backups/` directory of Plugin nodes from a VPS to a local machine.
+* Create a command using rsync to transfer backup files from a local machine to the `/plinode_backups/` directory of Plugin nodes simultaneously.
+* Please check if `/plinode_backups/` is available on the Plugin nodes before proceeding. If `/plinode_backups/` is not available, the command will not be executed.
+* Compatible with various encryption types. The following is a list of SSH key types that may be detected by the script:
+     * ssh-dss
+     * ssh-rsa
+     * ecdsa-sha2-nistp
+     * ssh-ed25519
+     * rsa-sha2-256
+     * ssh-x25519
+     * ssh-x448
+
+  Therefore, this script detects the key types of DSA, RSA, ECDSA, Ed25519, RSA-PSS, x25519, and x448.
+
+* Corresponds to both Japanese and English
+     * Created translations.csv to correspond to Japanese and English
+     * If necessary, I can also correspond to other languages, so please let me know.
+
+## Features
+1. Reads English and Japanese messages from a CSV file and displays them depending on the selected language.
+2. Create a command to transfer the `/plinode_backups/` directory of Plugin nodes to the local machine.
+3. At the same time, create a command to transfer backup files from the local machine to the `/plinode_backups/` directory of Plugin nodes.
+4. rsync also supports ssh-key.
+
+## About rsync
+rsync is a file synchronization tool that is mainly used to synchronize files/directories in different locations. rsync can synchronize files between a local machine and a remote machine, and transfer files over the network.
+
+The main feature of rsync is that it can transfer only the modified parts. That is, by calculating the differences between the source and destination files and transferring only the parts with differences, it operates very efficiently when transferring large amounts of data.
+
+In addition, rsync has other advantages such as:
+
+Ability to resume transfers from where they left off if interrupted
+Faster transfer of multiple files than scp in some cases
+Display of progress during transfers, making it easier to monitor transfer status
+The plugin node probably has rsync installed, which can be checked with rsync --version. If it is not installed, please run the following command to install it.
+```sh
+sudo apt install rsync
+```
+
+## Git Clone the repository
+
+Clone the repository, enter the pli_rsync directory, and change the permissions of pli_rsync.sh before running it.
+```sh
+git clone https://github.com/11ppm/pli_rsync
+cd pli_rsync
+chmod +x pli_rsync.sh
+```
+
+## Executing the script
+```sh
+./pli_rsync.sh
+```
+
+Select English or Japanese, and enter your password when prompted. 
+
+"IP Address:123.456.789.10" is an example.
+```sh
+Select a language 言語を選択してください :
+
+1. English
+2. 日本語
+
+Enter the number 数字を入力してください : 1
+
+[sudo] password for Doraemon: 
+
+                                       Your Plugin Node                                          
+-----------------------------------------------------------------------------------------------------
+IP Address           : 123.456.789.10
+User                 : Doraemon
+Backup Directory     : /plinode_backups/
+Local Directory      : ~/Documents/plugin_node/CT-TEST_123.456.789.10/plinode_backups/
+SSH Port             : 22
+SSH key type         : rsa
+-----------------------------------------------------------------------------------------------------
+
+
+                             Plugin Node    =====>>    Local Machine                             
+-----------------------------------------------------------------------------------------------------
+
+mkdir -p ~/Documents/plugin_node/CT-TEST_123.456.789.10/plinode_backups/ && rsync -avz --progress -e 'ssh -i ~/.ssh/id_rsa -p 22' Doraemon@123.456.789.10:/plinode_backups/ ~/Documents/plugin_node/CT-TEST_123.456.789.10/plinode_backups/
+
+-----------------------------------------------------------------------------------------------------
+To download backup file from the Plugin Node.run the following command in your local machine's terminal
+
+
+
+                             Plugin Node    <<=====    Local Machine                             
+-----------------------------------------------------------------------------------------------------
+
+rsync -avz --progress -e 'ssh -i ~/.ssh/id_rsa -p 22' ~/Documents/plugin_node/CT-TEST_123.456.789.10/plinode_backups/ Doraemon@123.456.789.10:/plinode_backups/
+
+-----------------------------------------------------------------------------------------------------
+To upload backup files from your local machine to the Plugin Node.run the following command in your terminal on the local machine.
+Note: Replace ~/.ssh/id_rsa with the appropriate path and filename of your private key.
+```
+
+## Local Machine (for Linux/Mac/WSL2)
+
+Open the terminal and paste and execute the script.
+
+Note: 
+* Please check if `/plinode_backups/` is available on the Plugin nodes before proceeding. If `/plinode_backups/` is not available, the command will not be executed.
+* Tested on Linux (Ubuntu), Mac, and WSL2 (Windows Subsystem for Linux 2), Windows 10 and 11 (with Cygwin).
+* If you are using Windows, it is recommended to install and run Ubuntu on WSL2, which is easy to set up. However, it is also possible to use `Cygwin`. In that case, please refer to the link below.
+
+     [Setting up Cygwin to use rsync on Windows](https://qiita.com/11ppm/private/3c5dddbcbc458400e62b)
+
+
+
+## Author
+
+* @11ppm
+
---
+++ goracle_backup.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+# 色の設定
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+RED='\033[1;31m' # 太字
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# CSVファイル名
+csv_file="./translations/translations.csv"
+
+# 言語の選択
+echo
+echo "Select a language 言語を選択してください :"
+echo
+echo "1. English"
+echo "2. 日本語"
+echo
+read -t30 -p "Enter the number 数字を入力してください : " lang_choice
+
+# 選択された言語に応じた列番号を設定
+case $lang_choice in
+1)
+    lang_col=2 # 英語列
+    ;;
+2)
+    lang_col=3 # 日本語列
+    ;;
+*)
+    echo "無効な選択です。"
+    exit 1
+    ;;
+esac
+
+# CSVファイルからメッセージを読み込み
+while read line; do
+    # キー名を取得
+    # cutコマンドで`|`を区切り文字とし、1列目のフィールドを抽出
+    key=$(echo $line | cut -d '|' -f 1)
+    # 選択された言語に対応するメッセージを取得
+    # awk -Fオプションを使ってフィールド区切り文字を`|`に指定、$で$lang_colで指定された列を取得
+    message=$(echo $line | awk -F '|' '{print $'$lang_col'}')
+    # 変数に格納
+    eval "$key=\"$message\""
+done <$csv_file
+
+echo
+# リモートサーバーのIPアドレスを取得する
+ip_address=$(ip addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '127.0.0.1' | head -n1)
+
+# リモートユーザーとして現在のユーザーを取得する
+remote_user=$(whoami)
+
+# リモートバックアップファイルを設定する
+backup_file=".goracle"
+
+# バックアップファイルを保存するローカルディレクトリを設定する
+local_dir="~/Documents/Goracle_node/$HOSTNAME"_"$ip_address/"
+
+# sshd_configファイルからsshポート番号を取得する
+ssh_port=$(sudo ss -tlpn | grep sshd | awk '{print$4}' | cut -d ':' -f 2 -s)
+if [[ -z "$ssh_port" ]]; then
+    ssh_port=22
+fi
+
+# SSHキーが存在するかを確認する
+sshkey_exists=0
+if grep -q "ssh-dss" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="dsa"
+    sshkey_exists=1
+elif grep -q "ssh-rsa" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="rsa"
+    sshkey_exists=1
+elif grep -q "ecdsa-sha2-nistp" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="ecdsa"
+    sshkey_exists=1
+elif grep -q "ssh-ed25519" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="ed25519"
+    sshkey_exists=1
+elif grep -q "rsa-sha2-256" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="rsa-pss"
+    sshkey_exists=1
+elif grep -q "ssh-x25519" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="x25519"
+    sshkey_exists=1
+elif grep -q "ssh-x448" ~/.ssh/authorized_keys 2>/dev/null; then
+    sshkey_type="x448"
+    sshkey_exists=1
+fi
+echo
+echo
+echo -e "${NC}                                       $YOUR_GORACLE_NODE                                          ${NC}"
+echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+printf "%-20s : %s\n" "$GORACLE_NODE_IP" "$ip_address"
+printf "%-20s : %s\n" "$GORACLE_NODE_USER" "$remote_user"
+printf "%-20s : %s\n" "$GORACLE_NODE_BACKUP_FILE" "$backup_file"
+printf "%-20s : %s\n" "$GORACLE_NODE_LOCAL_DIR" "$local_dir"
+printf "%-20s : %s\n" "$GORACLE_NODE_SSH_PORT" "$ssh_port"
+
+if [ $sshkey_exists -eq 1 ]; then
+    # SSHキーを使用してrsyncを実行する
+    # echo -e "$SSH_KEY_DETECTED : $sshkey_type"
+    printf "%-20s : %s\n" "$SSH_KEY_DETECTED" "$sshkey_type"
+    echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo -e "${YELLOW}                             $REMOTE_SERVER    =====>>    $LOCAL_MACHINE                             ${NC}"
+    echo -e "${YELLOW}-----------------------------------------------------------------------------------------------------${NC}"
+    echo
+    private_key="~/.ssh/id_${sshkey_type}"
+    # ローカルディレクトリがなければ作成
+    echo -e "${YELLOW}mkdir -p $local_dir && rsync -avz --progress -e 'ssh -i ${NC}${RED}${private_key}${NC}${YELLOW} -p ${ssh_port}' ${remote_user}@${ip_address}:$backup_file ${local_dir}${NC}"
+    echo
+    echo -e "${YELLOW}-----------------------------------------------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}$DOWNLOAD_BACKUP${NC}"
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo -e "${NC}                             $REMOTE_SERVER    <<=====    $LOCAL_MACHINE                             ${NC}"
+    echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+    echo
+    echo -e "${NC}rsync -avz --progress -e 'ssh -i ${RED}${private_key}${NC} -p ${ssh_port}' ${local_dir}${backup_file} ${remote_user}@${ip_address}:$backup_file${NC}"
+    echo
+    echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+    echo -e "${NC}$UPLOAD_BACKUP${NC}"
+    echo
+    echo -e "${NC}$NOTE: $REPLACE${NC} ${RED}${private_key}${NC}${NC} $PRIVATE_KEY_PATH_FILENAME${NC}"
+    echo
+    echo
+    echo
+    echo
+    echo
+else
+    # ※ SSHキーを使用せずにrsyncを実行する
+    # printf "%-20s : %s\n" "$SSH_KEY_NOT_DETECTED" "$BLANK"
+    printf "%-20s \n" "$SSH_KEY_NOT_DETECTED"
+    echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo -e "${YELLOW}                             $REMOTE_SERVER    =====>>    $LOCAL_MACHINE                             ${NC}"
+    echo -e "${YELLOW}-----------------------------------------------------------------------------------------------------${NC}"
+    echo
+    echo -e "${YELLOW}mkdir -p $local_dir && rsync -avz --progress -e 'ssh -p ${ssh_port}' ${remote_user}@${ip_address}:$backup_file ${local_dir}${NC}"
+    echo
+    echo -e "${YELLOW}-----------------------------------------------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}$DOWNLOAD_BACKUP${NC}"
+    echo
+    echo
+    echo
+    echo
+    echo
+    echo -e "${NC}                             $REMOTE_SERVER    <<=====    $LOCAL_MACHINE                             ${NC}"
+    echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+    echo
+    echo -e "${NC}rsync -avz --progress -e 'ssh -p ${ssh_port}' ${local_dir}${backup_file} ${remote_user}@${ip_address}:$backup_file${NC}"
+    echo
+    echo -e "${NC}-----------------------------------------------------------------------------------------------------${NC}"
+    echo -e "${NC}$UPLOAD_BACKUP${NC}"
+    echo
+    echo
+    echo
+    echo
+    echo
+fi
